be03be3bc2a0953a64fc284a97dae59c80b781ac
wanwenkai <wanwenkai@ict.ac.cn>
2020-03-29 21:12:07 +0800

    20200329. a new version about emulator. This version update hybrid slow start

--- server/pqsa_server.cpp
+++ server/pqsa_server.cpp
@@ -1,25 +1,22 @@
 #include "pqsa.hpp"
-
+#include <queue>
 /* hybrid slow start */
 #define HYSTART_ACK_TRAIN   0x1
 #define HYSTART_DELAY       0x2
-#define HYSTART_MIN_SAMPLES 8
 #define HYSTART_DELAY_MIN   (4u<<3)
 #define HYSTART_DELAY_MAX   (16u<<3)
 
 #define HYSTART_DELAY_THRESH clamp(x, HYSTART_DELAY_MIN, HYSTART_DELAY_MAX)
-//static int hystart __read_mostly = 1;
-static int hystart_detect = HYSTART_ACK_TRAIN | HYSTART_DELAY;
+static uint8_t hystart_detect = HYSTART_ACK_TRAIN | HYSTART_DELAY;
 static int hystart_low_window  = 16;
 static uint32_t hystart_ack_delta = 2;
 /* end */
 
-
-#define MTU 1460
-#define PACKET_SIZE (MTU+54)
+#define MTU 1500
+#define PACKET_SIZE MTU
 #define DELTA 2
 
-#define T_THRESHOLD 20
+#define T_THRESHOLD 20    //parameter
 #define RESOLUTION 1000
 /* state val */
 bool rtt_detect = true;
@@ -32,35 +29,38 @@ int s, err;
 /* session id */
 int ss_id;
 
-/* epoch count */
-int epoch_num;
-
-int M = 5;
+int M = 3;   //parameter
+int tau = 3;  //parameter
 
 uint32_t crt_win = 1;
+uint32_t sender_pact = 2;
 uint32_t ss_crt_win = 1;
-
+int slow_epoch = 1;
 socklen_t len_inet;
 
+double alpha = 0.8;
+uint32_t cur_rtt = UINT_MAX;
 uint32_t latest_rtt = UINT_MAX;
 uint32_t min_rtt = UINT_MAX;
-double sending_rate;//sending rate at each epoch
-double hybrid_quit_rate;//quit rate at the end of slow start
-double s_gamma;//rate factor
-
+double sending_rate;
+double sending_rate_latest;
+double s_gamma;
+double sigma = 0.5;
 
 /* packet sequence */
-uint64_t pkt_seq;
-uint64_t rcv_bytes;
+int pkt_seq;
+int rcv_bytes;
 
 struct hybridss s_hyss;
 
 struct sockaddr_in adr_clnt;
-struct sockaddr_in adr_clnt2;
+struct sockaddr_in adr_clnt2; 
 
 /* the time of program start run */
 struct timeval start_time;
 struct timeval last_ack_time;
+
+std::queue<uint32_t> t_win;
 
 pthread_mutex_t sendingWinLock;
 pthread_mutex_t sendingCntLock;
@@ -100,57 +100,71 @@ static inline uint32_t pqsa_clock(void)
 static void hystart_reset(struct hybridss *hyss)
 {
     hyss->round_start = hyss->last_ack = pqsa_clock();
-    hyss->curr_rtt = 0;
-    hyss->sample_cnt = 0;
+	
+	FILE *fp;
+	double run_time = 0;
+	struct timeval current_time;
+	gettimeofday(&current_time,NULL);
+	run_time = (current_time.tv_sec - start_time.tv_sec) + (current_time.tv_usec - start_time.tv_usec)/1000000.0;
+	fp = fopen("einfo_slow_exit.txt", "a");	
+	fprintf(fp, "reset epoch = %d t = %.3f\n", 
+				slow_epoch -1, run_time);
+	fclose(fp);
 }
 
 static void update_sending_rate()
 {
-    uint64_t est_byte;
+	if (latest_rtt >= min_rtt+T_THRESHOLD)
+		s_gamma = (min_rtt*1.0+T_THRESHOLD)/latest_rtt;
+	else if (latest_rtt > min_rtt + T_THRESHOLD*sigma)
+		s_gamma = 1.0;
+	else if (latest_rtt <= min_rtt+T_THRESHOLD*sigma)
+		s_gamma = 1.0 + (min_rtt + T_THRESHOLD*sigma - latest_rtt)*1.0/min_rtt;
 
-    s_gamma = (latest_rtt > min_rtt + T_THRESHOLD)?(min_rtt*1.0/latest_rtt):s_gamma;
-
-    if (epoch_num < M) {
-        est_byte = hybrid_quit_rate*(M - epoch_num + 1)/(M*DELTA) + rcv_bytes;
-
-        pthread_mutex_lock(&sendingCntLock);
-        sending_rate = s_gamma*est_byte*RESOLUTION/(M*DELTA);
-        pthread_mutex_unlock(&sendingCntLock);
-    } else {
-
-        pthread_mutex_lock(&sendingCntLock);
-        sending_rate = s_gamma*rcv_bytes*RESOLUTION/(M*DELTA);
-        pthread_mutex_unlock(&sendingCntLock);
-    }
+   	pthread_mutex_lock(&sendingCntLock);
+		
+	sending_rate = s_gamma*rcv_bytes*RESOLUTION/(M*DELTA);
 
 	pthread_mutex_lock(&restartLock);
-    rcv_bytes = 0;
-	pthread_mutex_unlock(&restartLock);
+	rcv_bytes = 0;
+    pthread_mutex_unlock(&restartLock);
+
+    pthread_mutex_unlock(&sendingCntLock);
 }
 
-static void hystart_update(struct hybridss *hyss, uint32_t delay)
+static void hystart_update(struct hybridss *hyss, uint32_t delay, int seq)
 {
 
     if (!(hyss->found & hystart_detect)) {
         uint32_t now = pqsa_clock();
-    
         /* */
+		FILE *fp;
+		double run_time = 0;
+		struct timeval current_time;
+		gettimeofday(&current_time,NULL);
+		run_time = (current_time.tv_sec - start_time.tv_sec) + (current_time.tv_usec - start_time.tv_usec)/1000000.0;
+		fp = fopen("einfo_slow_exit.txt", "a");	
         if ((now - hyss->last_ack) <= hystart_ack_delta) {
-            hyss->last_ack = now; 
-
-            if ((now - hyss->round_start) > hyss->delay_min >> 4)
-                hyss->found |= HYSTART_ACK_TRAIN;
-        }
-        
+            hyss->last_ack = now;
+		   	fprintf(fp, "ack found = %u t = %.3f ss_crt_win = %u now-start = %u slowstart_min = %u\n", 
+						uint32_t(hyss->found), run_time, ss_crt_win, now - hyss->round_start, hyss->delay_min);
+			//Divide by 16	
+            if ((now - hyss->round_start) > hyss->delay_min >> 4){
+				hyss->found |= HYSTART_ACK_TRAIN;
+				fprintf(fp, "first way found = %u t = %.3f ss_crt_win = %u slowstart_min = %u now-start = %u\n", 
+						uint32_t(hyss->found), run_time, ss_crt_win, hyss->delay_min, now - hyss->round_start);
+			}
+		}else{
         /* obtain the mininum delay of more than sampling packets */
-        if (hyss->sample_cnt < HYSTART_MIN_SAMPLES) {
-            if (hyss->curr_rtt == 0 || hyss->curr_rtt > delay)
-                hyss->curr_rtt = delay;
-            hyss->sample_cnt++;
-        } else {
-            if (hyss->curr_rtt > hyss->delay_min + Clamp(hyss->delay_min >> 4))
-                hyss->found |= HYSTART_DELAY;
-        }
+			fprintf(fp, "delay found = %u t = %.3f ss_crt_win = %u cur_rtt = %u slowstart_min = %u Clamp = %lu pdu_seq=%d\n", 
+					uint32_t(hyss->found), run_time, ss_crt_win, delay, hyss->delay_min, Clamp(hyss->delay_min >> 4), seq);
+            if (delay > hyss->delay_min + Clamp(hyss->delay_min >> 4)){
+				hyss->found |= HYSTART_DELAY;
+				fprintf(fp, "first way found = %u\n", uint32_t(hyss->found));
+			}
+		
+		}
+		fclose(fp);
     }
 
     if (hyss->found & hystart_detect) {
@@ -181,11 +195,11 @@ static void restart_session()
     pkt_seq = 0;
     sending_rate = 10.0;
     ss_id++;
-    epoch_num = 0;
     rcv_bytes = 0;
-    s_gamma = 1.0;
+    s_gamma = 1.0;   //parameter
 
     hystart_reset(&s_hyss);
+
     //pthread_mutex_unlock(&restartLock);
 }
 
@@ -219,18 +233,33 @@ void* sending_thread(void *arg)
     int i, ret;
     int n_packets;
     udp_packet_t *pdu;
-    
+   	double sum_packets = 0.0;
+	
     while (!terminate) {
         while (slow_start && crt_win > 0) {
-            n_packets = crt_win;
-
-            pthread_mutex_lock(&sendingWinLock);
+            //first send one packet, then send two packets after receiving one ack 
+			if (crt_win == 1){
+				n_packets = crt_win;
+			}else{
+				n_packets = sender_pact;
+			}
+			pthread_mutex_lock(&sendingWinLock);
             crt_win = 0;
             pthread_mutex_unlock(&sendingWinLock);
 
             for (i = 0; i < n_packets; i++) {
                 pkt_seq ++;
                 pdu = udp_pdu_init(pkt_seq, MTU, ss_id);
+				
+				FILE *fp;
+				double run_time = 0;
+				struct timeval current_time;
+				gettimeofday(&current_time,NULL);
+				run_time = (current_time.tv_sec - start_time.tv_sec) + (current_time.tv_usec - start_time.tv_usec)/1000000.0;
+				fp = fopen("einfo_slow_sendpact.txt", "a");	
+				fprintf(fp, "t=%.4f pdu_seq=%d\n", run_time, int(pdu->seq));
+				fclose(fp);
+
                 ret = sendto(s, pdu, MTU, MSG_DONTWAIT, 
                         (struct sockaddr *)&adr_clnt, len_inet);
                 if (ret < 0) {
@@ -242,6 +271,17 @@ void* sending_thread(void *arg)
                         if (slow_start) {
                             printf("exit slow start\n");
                             slow_start = false;
+							
+							FILE *fp;
+							double run_time = 0;
+							struct timeval current_time;
+							gettimeofday(&current_time,NULL);
+							run_time = (current_time.tv_sec - start_time.tv_sec) + (current_time.tv_usec - start_time.tv_usec)/1000000.0;
+							fp = fopen("einfo_slow_exit.txt", "a");	
+							fprintf(fp, "error t = %.3f min = %u cur = %u\n", 
+									run_time, min_rtt, latest_rtt);
+							fclose(fp);
+							
                             pkt_seq --;
                             free(pdu);
                             break;
@@ -257,12 +297,20 @@ void* sending_thread(void *arg)
                 free(pdu);
             }
         }
-
+		
+		double tmp = 0.0;
         /* congestion control */
         while (!slow_start && sending_rate > 0.0) {
-            n_packets = fmax(2, sending_rate*M*DELTA/(1000*PACKET_SIZE));
-        //    printf("n_packets = %d\n", n_packets);
-            
+            int n_packets = 0;
+			tmp = sending_rate*1.0*tau*DELTA/(1000*PACKET_SIZE);
+
+			if (n_packets < 1)
+           		sum_packets += tmp;
+
+			if (sum_packets >= 1.0) {
+				n_packets = (int)sum_packets;
+				sum_packets -= n_packets;
+			}
             pthread_mutex_lock(&sendingCntLock);
             sending_rate = 0.0;
             pthread_mutex_unlock(&sendingCntLock);
@@ -287,10 +335,8 @@ void* sending_thread(void *arg)
 void timeout_handler(const boost::system::error_code& e)
 {
     if (e) return;
-
     update_sending_rate();
-
-    timer.expires_from_now (boost::posix_time::milliseconds(DELTA*M));
+    timer.expires_from_now (boost::posix_time::milliseconds(DELTA*tau));
     timer.async_wait(&timeout_handler);
 
     return;
@@ -310,8 +356,6 @@ void *timeout_thread(void *arg)
 
 void* receiver_thread(void *arg)
 {
-    uint32_t i;
-    int cnt = 0;
     double hyss_rate = 0.0;
     socklen_t len_inet;
     udp_packet_t *pdu;
@@ -319,10 +363,8 @@ void* receiver_thread(void *arg)
     static struct timeval last_time;
 
     len_inet = sizeof(struct sockaddr_in);
-
     pdu = (udp_packet_t *)malloc(sizeof(udp_packet_t));
     while (!terminate) {
-
         if (recvfrom(s, pdu, sizeof(udp_packet_t), 0, 
                     (struct sockaddr *)&adr_clnt, &len_inet) < 0)
             displayError("Receiver thread error");
@@ -335,14 +377,24 @@ void* receiver_thread(void *arg)
             exit(-1);
         }
         
+        gettimeofday(&received_time, NULL);
         pthread_mutex_lock(&restartLock);
+		
+		while (!t_win.empty() && (t_win.back() - t_win.front() > M*DELTA))
+			t_win.pop();
 
-        rcv_bytes += PACKET_SIZE;
+		t_win.push(received_time.tv_sec*1000+received_time.tv_usec/1000);
+        rcv_bytes = t_win.size()*PACKET_SIZE;
+
 		pthread_mutex_unlock(&restartLock);
 
-        gettimeofday(&received_time, NULL);
-        latest_rtt = (received_time.tv_sec - pdu->seconds)*1000 + 
+        cur_rtt = (received_time.tv_sec - pdu->seconds)*1000 + 
             (received_time.tv_usec - pdu->millis)/1000;
+
+		if (latest_rtt == UINT_MAX)
+			latest_rtt = cur_rtt;
+		else
+			latest_rtt = (1-alpha)*latest_rtt + alpha*cur_rtt;
        
         if (min_rtt > latest_rtt) {
             min_rtt = latest_rtt;
@@ -362,17 +414,19 @@ void* receiver_thread(void *arg)
          * */
         if (slow_start) {
             ss_crt_win += 1;
-            pthread_mutex_lock(&sendingWinLock);
+            
+			pthread_mutex_lock(&sendingWinLock);
             crt_win = ss_crt_win;
             pthread_mutex_unlock(&sendingWinLock);
 
-            if ((received_time.tv_sec - last_time.tv_sec)*1000 + 
-                (received_time.tv_usec - last_time.tv_usec)/1000 > latest_rtt) {
-                last_time.tv_sec = received_time.tv_sec;
-                last_time.tv_usec = received_time.tv_usec;
-                hystart_reset(&s_hyss);
-            }
-            hystart_update(&s_hyss, latest_rtt);
+            hystart_update(&s_hyss, latest_rtt, pdu->seq);
+			if (slow_start){
+				int index = int(pow(2, slow_epoch));
+				if ((ss_crt_win % index) == 0){
+					slow_epoch += 1;
+					hystart_reset(&s_hyss);
+				}
+			}
         }
 
         //pthread_mutex_unlock(&restartLock);
@@ -382,7 +436,7 @@ void* receiver_thread(void *arg)
 int main(int argc, char **argv)
 {
     int i = 0, port;
-    double relative_time = 0;
+    double relative_time = 0.0;
     double time_to_run;
     char dgram[512];
     char *name;
@@ -439,7 +493,7 @@ int main(int argc, char **argv)
 
     restart_session();
     gettimeofday(&start_time, NULL);
-
+	
     /* starting the threads */
     if (pthread_create(&(timeout_tid), NULL, &timeout_thread, NULL) != 0)
         std::cout << "can't create thread: " << strerror(err) << "\n";
@@ -451,19 +505,10 @@ int main(int argc, char **argv)
     std::cout << "Client " << port << " is connected\n";
     
     gettimeofday(&tempstamp, NULL);
+	
     while (relative_time <= time_to_run) {
         gettimeofday(&current_time, NULL);
-
         relative_time = (current_time.tv_sec - start_time.tv_sec) + 
             (current_time.tv_usec - start_time.tv_usec)/1000000.0;
-        
-        if ((current_time.tv_sec - tempstamp.tv_sec)*1000 + 
-                (current_time.tv_usec - tempstamp.tv_usec)/1000 > DELTA) {
-            tempstamp = current_time;
-            epoch_num++;
-        }
-
-
-        /* waiting for slow start to finish to start sending data */
-    }
+   	}
 }
